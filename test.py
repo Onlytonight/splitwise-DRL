571945df7dbb72bcc2a3e90b26e84cd98848b686
Onlytonight <1198076162@qq.com>
2025-11-15 23:03:47 +0800

    graph

---
+++ test.py
@@ -0,0 +1,10 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+ 
+@hydra.main(version_base=None, config_path="configs", config_name="config.yaml")
+def my_app(cfg: DictConfig) -> None:
+    #print(OmegaConf.to_yaml(cfg))
+    pass
+ 
+if __name__ == "__main__":
+    my_app()
