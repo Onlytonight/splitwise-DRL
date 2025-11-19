3e80ea2ab56ce5aae69e3dc8c46e3d8cbc67c93f
onlyTonight <1198076162@qq.com>
2025-11-19 23:51:31 +0800

    '1'

--- scheduler.py
+++ scheduler.py
@@ -777,7 +777,7 @@ class AdaptiveMixedPoolScheduler(KVScheduler):
         self.interval_ttft_stats = []  # 存储两次schedule调用间的TTFT统计
         from notebooks.perf_model import PerfModel
         # self.perf_model = PerfModel("D:\homework\网络\论文\LLMshedule\pd分离\splitwise-DRL\data\perf_model.csv", init=True)
-        self.perf_model = PerfModel("D:\homework\网络\论文\LLMshedule\pd分离\splitwise-DRL\data\perf_model.csv", init=True)
+        self.perf_model = PerfModel("/home/xfusion\conda\splitwise-DRL\data\perf_model.csv", init=True)
 
 
     def is_memory_loaded(self, instance, tasks):
