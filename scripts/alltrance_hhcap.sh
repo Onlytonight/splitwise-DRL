5fdc71a006fda03745f5cc4034045850cd57efa8
onlyTonight <1198076162@qq.com>
2025-11-24 17:42:31 +0800

    '1'

--- scripts/alltrance_hhcap.sh
+++ scripts/alltrance_hhcap.sh
@@ -1,7 +1,7 @@
 TRACE_code=rr_code_30,rr_code_40,rr_code_50,rr_code_60,rr_code_70,rr_code_80,rr_code_90,rr_code_100,rr_code_110,rr_code_120,rr_code_130,rr_code_140
 # ,rr_code_150,rr_code_160,rr_conv_170,rr_conv_180
-TRACE_conv=rr_conv_30,rr_conv_40,rr_conv_50,rr_conv_60,rr_conv_70,rr_conv_80,rr_conv_90,rr_conv_100,rr_conv_110,rr_conv_120,rr_conv_130,rr_conv_140
-# ,rr_conv_150,rr_conv_160,rr_conv_170,rr_conv_180
+TRACE_conv=rr_conv_30,rr_conv_40,rr_conv_50,rr_conv_60,rr_conv_70,rr_conv_80,rr_conv_90,rr_conv_100,rr_conv_110,rr_conv_120,rr_conv_130,rr_conv_140,rr_conv_150
+# ,rr_conv_160,rr_conv_170,rr_conv_180
 SEED=0
 
 python run.py --multirun\
