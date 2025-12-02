ce6feb7ad168bb4393bc4f4c02c9621a8c41369c
onlyTonight <1198076162@qq.com>
2025-12-02 15:59:17 +0800

    '1'

--- scripts/alltrance_hhcap.sh
+++ scripts/alltrance_hhcap.sh
@@ -16,7 +16,7 @@ python run.py --multirun\
     start_state.prompt.num_instances=25 \
     start_state.token.num_instances=15 \
     performance_model=db \
-    trace.filename=$TRACE_mix \
+    trace.filename=$TRACE_code \
     seed=0
 
 #--multirun    applications.0.scheduler=adaptive_pool     cluster=half_half     cluster.servers.0.count=0     cluster.servers.1.count=40     start_state=splitwise     start_state.prompt.num_instances=25     start_state.token.num_instances=15     performance_model=db     trace.filename=TRACE_conv=rr_conv_30,rr_conv_40,rr_conv_50,rr_conv_60,rr_conv_70,rr_conv_80,rr_conv_90,rr_conv_100,rr_conv_110,rr_conv_120,rr_conv_130,rr_conv_140    seed=0
